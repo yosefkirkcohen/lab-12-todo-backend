d8bfd371b64496730049c3fb10ce298987f25e5e
yosefkirkcohen <ycohen45@yahoo.com>
2021-10-11 14:45:59 -0700

    get worked

--- data/todos.js
+++ data/todos.js
@@ -2,20 +2,17 @@ module.exports = [
   {
     id:  1,
     todo: 'bla  the bla',
-    completed: false,
-    user_id: 5
+    completed: false
   },
   {
     id:  2,
     todo: 'ok okok ok',
-    completed: false,
-    user_id: 5
+    completed: false
   },
   {
     id:  3,
     todo: 'yada yada yada',
-    completed: false,
-    user_id: 5
+    completed: false
   },
   
 ];
